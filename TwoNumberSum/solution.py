7589e0469633bd6c9f8ce25337b5b99c96f1aa7c
murat.tulgac <murat.tulgac@issd.com.tr>
2022-02-07 20:09:05 +0300

    fix: indentation difference is solved.

--- TwoNumberSum/solution.py
+++ TwoNumberSum/solution.py
@@ -1,10 +1,10 @@
 def twoNumberSum(array, targetSum):
     # Write your code here.
     reverseArray = []
-	result = []
-	for i in array:
-		reverseArray.append(targetSum-i)
-	for i in range(len(reverseArray)):
-		if (reverseArray[i] in array) and (reverseArray[i]!=array[i]):
-			result = [array[i], reverseArray[i]]
-	return result
+    result = []
+    for i in array:
+        reverseArray.append(targetSum-i)
+    for i in range(len(reverseArray)):
+        if (reverseArray[i] in array) and (reverseArray[i]!=array[i]):
+            result = [array[i], reverseArray[i]]
+    return result
