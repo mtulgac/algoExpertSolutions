d4fe924cd52a793107949b0eb30c2f6990ec0088
mtulgac <muratulgac@gmail.com>
2022-02-07 20:19:12 +0300

    refactor: comments are added.

--- TwoNumberSum/solution.py
+++ TwoNumberSum/solution.py
@@ -1,9 +1,14 @@
 def twoNumberSum(array, targetSum):
-    # Write your code here.
+
+    # Defining two arrays for result and comparison
     reverseArray = []
     result = []
+
+    # Appending the required integers for targetSum
     for i in array:
         reverseArray.append(targetSum-i)
+
+    # Checking if any of the required integers are in the original array
     for i in range(len(reverseArray)):
         if (reverseArray[i] in array) and (reverseArray[i]!=array[i]):
             result = [array[i], reverseArray[i]]
